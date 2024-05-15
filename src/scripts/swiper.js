8407de9e77e59a540fa997214ddc68a97897bef6
Nirjla <shakyanirjala6@gmail.com>
2024-05-15 22:45:43 +0545

    made responsive for mobile-sizes

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -36,3 +36,27 @@ var swiper2 = new Swiper(".mySwiper2", {
     clickable: true,
   },
 });
+
+
+var swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
